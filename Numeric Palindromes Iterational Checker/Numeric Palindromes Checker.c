dfcc93ac60a1310a015f260bfc60d9723572b0c6
aescaesse <54239071+aescaesse@users.noreply.github.com>
2019-08-24 22:43:52 +0200

    Update

---
+++ Numeric Palindromes Iterational Checker/Numeric Palindromes Checker.c
@@ -0,0 +1,71 @@
+﻿#include <stdio.h> 
+#include <time.h> 
+#include <stdlib.h> 
+#include <conio.h> 
+#include <math.h>
+#include <float.h>
+#include <stdbool.h>
+#include <windows.h>
+
+unsigned long long int reverse = 0, n, m, Check, CheckL,k, tmp;
+void Reverse()
+{
+	m = n;
+	reverse = 0;
+	while (m != 0)
+	{
+		int last = m % 10;
+		reverse = reverse * 10 + last;
+		m /= 10;
+	}
+	
+}
+
+void AddReversed()
+{
+	n += reverse;
+}
+
+int IsPalindrome()
+{
+	k = n;
+	m = k;
+	Check = 0;
+	while (m != 0)
+	{
+		CheckL = m % 10;
+		Check = Check * 10 + CheckL;
+		m /= 10;
+	}
+	if (k==Check)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void Core()
+{
+	while (!IsPalindrome())
+	{
+		Reverse();
+		AddReversed();
+	}
+	printf("For number %lli : %lli + %lli = %lli\n", tmp, n, reverse, k);
+}
+
+int main()
+{
+	tmp = 10;
+	while (1)
+	{	
+		n = tmp;
+		Core();
+		tmp++;
+	}
+	
+	return 0;
+}
---
+++ Numeric Palindromes Iterational Checker/README.md
@@ -0,0 +1,7 @@
+In the April 1984 Scientific American "Computer Recreations" column, an article appeared about mathematical patterns (F. Gruenberger, Computer Recreations, "How to Handle Numbers with Thousands of Digits, and Why One Might Want To.", Scientific American, 250 [No. 4, April, 1984], 19-26.). 
+Here's the algorithm:
+Pick a number.
+Reverse its digits and add this value to the original number.
+If this is not a palindrome, go back to step 2 and repeat.
+
+Do all numbers eventually become palindromes by this process? It was suggested that this is the case.
