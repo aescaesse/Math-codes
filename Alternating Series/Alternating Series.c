bce546e91635d1c46b2c22521bc618c51eeb2377
aescaesse <54239071+aescaesse@users.noreply.github.com>
2019-08-28 22:31:15 +0200

    Update

---
+++ Alternating Series/Alternating Series.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h> 
+#include <time.h> 
+#include <stdlib.h> 
+#include <conio.h> 
+#include <math.h>
+#include <float.h>
+#include <stdbool.h>
+#include <windows.h>
+
+long double Value;
+int n;
+
+long double Factorial(int n)
+{
+	long double factorial = 1;
+	for (int i = 1; i <= n +1; ++i)
+	{
+		factorial *= i;
+	}
+	return factorial;
+}
+
+void Core()
+{
+	n = 0;
+	long double F;
+	while (1)
+	{
+		F=Factorial(n);
+		Value = (pow(-1, n)) / F;
+		printf("%.250Lf \n", Value);
+		n++;
+		Sleep(100);
+	}
+	
+}
+
+int main()
+{
+	Core();
+	return 0;
+}
