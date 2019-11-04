4ca8afc09f5f50f834a75b5c0509d050e9a9c67c
aescaesse <54239071+aescaesse@users.noreply.github.com>
2019-11-04 11:50:24 +0100

    Create amicablenumbrs.cpp

---
+++ Amicable numbers - force method/amicablenumbrs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int DivisorsSum(int n) {
+	int Sum = 1;
+	for (int i = 2; i < n; i++)
+		if (n % i == 0)
+			Sum += i;
+	return Sum;
+}
+
+int isAmicable(int a) {
+	int b = DivisorsSum(a);
+	return (a == DivisorsSum(b) && a != b) ? b : 0;
+}
+
+void FromRange(int od, int b) {
+	int t, i = od, ifExist = 0;
+	while (i < b) {
+		if ((t = isAmicable(i)) != 0 && i < t) {
+			cout << i << " " << t << endl;
+			ifExist = 1;
+		}
+		i++;
+	}
+	if (ifExist == 0)
+	{
+		cout << "Nothing";
+	}
+}
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << "Amicable numbers in <" << a << ", " << b << ">" << endl;
+	FromRange(a, b);
+	return 0;
+}
